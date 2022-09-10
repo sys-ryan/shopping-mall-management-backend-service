285501233e83643c7b676e72ad5042d6d7814f22
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-11 00:39:36 +0900

    feat: deliveries entity 생성
    - deliveries entity 생성
    - entities column name 오류 수정

--- src/common/enums.ts
+++ src/common/enums.ts
@@ -3,3 +3,13 @@ export enum COUPONE_TYPE_ENUM {
   PERCENTAGE = "percentage",
   SHIPPING_FEE = "shipping_fee",
 }
+
+export enum DELIVERY_STATUS_ENUM {
+  SHIPPING = "shipping",
+  COMPLETED = "completed",
+}
+
+export enum PAY_STATE_ENUM {
+  COMPLETED = "completed",
+  CANCELED = "canceled",
+}
--- src/resources/orders/entities/orders.entity.ts
+++ src/resources/orders/entities/orders.entity.ts
@@ -1,12 +1,111 @@
+import { PAY_STATE_ENUM } from "src/common/enums";
 import { Countries } from "src/resources/countries/entities/countries.entity";
 import { Coupons } from "src/resources/coupons/entities/coupons.entity";
+import { Deliveries } from "src/resources/deliveries/entities/deliveries.entity";
 import { Users } from "src/resources/users/entities/users.entity";
-import { Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
 
 @Entity()
 export class Orders {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({
+    type: "enum",
+    name: "pay_state",
+    enum: PAY_STATE_ENUM,
+    comment: "해당 컬럼은 주문에 대한 결제 완료 여부를 나타냅니다.",
+  })
+  payState: PAY_STATE_ENUM;
+
+  @Column({ type: "int", comment: "해당 컬럼은 주문한 상품의 개수를 나타냅니다." })
+  quantity: number;
+
+  @Column({
+    type: "decimal",
+    name: "original_price",
+    precision: 8,
+    scale: 2,
+    comment: "해당 컬럼은 할인 전 상품의 가격을 나타냅니다.",
+  })
+  originalPrice: number;
+
+  @Column({
+    type: "decimal",
+    name: "discounted_price",
+    precision: 8,
+    scale: 2,
+    comment: "해당 컬럼은 상품 할인 쿠폰을 적용했을 경우 할인되는 금액을 나타냅니다.",
+    default: 0,
+  })
+  discountedPrice: number;
+
+  @Column({
+    type: "decimal",
+    name: "final_price",
+    precision: 8,
+    scale: 2,
+    comment:
+      "해당 컬럼은 상품할인 쿠폰을 적용했을 경우, 주문자가 지불해야할 상품의 금액을 나타냅니다.",
+  })
+  finalPrice: number;
+
+  @Column({
+    type: "decimal",
+    name: "discounted_delivery_cost",
+    precision: 8,
+    scale: 2,
+    comment: "해당 컬럼은 배송비 할인 쿠폰을 적용했을 경우, 할인되는 배송비를 나타냅니다.",
+    default: 0,
+  })
+  discountedDeliveryCost: number;
+
+  @Column({
+    type: "decimal",
+    name: "final_delivery_cost",
+    precision: 8,
+    scale: 2,
+    comment:
+      "해당 컬럼은 배송비 할인 쿠폰을 적용했을 경우, 주문자가 지불해야할 배송비를 나타냅니다.",
+  })
+  finalDeliveryCost: number;
+
+  @Column({
+    type: "decimal",
+    name: "total_price",
+    precision: 8,
+    scale: 2,
+    comment:
+      "해당 컬럼은 상품할인 또는 배송비 할인이 적용된 상품 가격과 배송비를 모두 합한 가격을 나타냅니다.",
+  })
+  totalPrice: number;
+
+  @Column({ type: "varchar", length: 20, comment: "해당 컬럼은 배송지 우편번호를 나타냅니다." })
+  buyr_zipx: string;
+
+  @Column({ type: "varchar", length: 50, comment: "해당 컬럼은 배송지 도시를 나타냅니다." })
+  buyr_city: string;
+
+  @Column({ type: "int", comment: "해당 컬럼은 vccode를 나타냅니다." })
+  vccode: number;
+
+  @CreateDateColumn({ comment: "해당 컬럼은 주문이 생성된 시간을 나타냅니다." })
+  createdAt: Date;
+
+  @UpdateDateColumn({ comment: "해당 컬럼은 주문이 수정된 시간을 나타냅니다." })
+  updatedAt: Date;
+
+  @Column({ comment: "해당 컬럼은 주문이 취소된 시간을 나타냅니다." })
+  canceledAt: Date;
 
   @ManyToOne(() => Users, (user) => user.orders)
   user: Users;
@@ -17,4 +116,7 @@ export class Orders {
   @OneToOne(() => Coupons, (coupon) => coupon.order)
   @JoinColumn()
   coupon: Coupons;
+
+  @OneToOne(() => Deliveries, (delivery) => delivery.order)
+  delivery: Deliveries;
 }
