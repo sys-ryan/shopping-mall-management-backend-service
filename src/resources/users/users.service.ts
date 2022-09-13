0e700d20dcd906149bd1912a7e71cdfc1f3cdab6
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-13 17:39:24 +0900

    refactor: 불필요한 코드 제거

--- src/resources/users/users.service.ts
+++ src/resources/users/users.service.ts
@@ -2,7 +2,6 @@ import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { CreateUserDto } from "./dto/create-user.dto";
-import { UpdateUserDto } from "./dto/update-user.dto";
 import { Users } from "./entities/users.entity";
 
 @Injectable()
