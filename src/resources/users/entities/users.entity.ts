dd2f09e1c2045a6e134a0c5c42d3335b9d7be4cc
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-14 03:52:44 +0900

    feat: users api 구현

--- src/resources/users/entities/users.entity.ts
+++ src/resources/users/entities/users.entity.ts
@@ -9,9 +9,17 @@ export class Users {
   @Column({
     type: "varchar",
     length: 50,
-    comment: "해당 컬럄안 사용자(주문자)의 이름을 나타냅니다.",
+    comment: "해당 컬럄은 사용자(주문자)의 이름을 나타냅니다.",
   })
   name: string;
+
+  @Column({
+    type: "boolean",
+    name: "is_deleted",
+    comment: "해당 컬럼은 사용자 삭제 여부를 나타냅니다.",
+    default: false,
+  })
+  isDeleted: boolean;
 
   @OneToMany(() => Orders, (order) => order.user)
   @JoinColumn()
--- src/resources/users/users.controller.ts
+++ src/resources/users/users.controller.ts
@@ -1,29 +1,40 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from "@nestjs/common";
 import { UsersService } from "./users.service";
-import { CreateUserDto } from "./dto/create-user.dto";
-import { UpdateUserDto } from "./dto/update-user.dto";
+import { CreateUserDto, CreateUserResponseDto } from "./dto/create-user.dto";
+import { Users } from "./entities/users.entity";
+import { DeleteUserResponseDto } from "./dto/delete-user.dto";
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 
+@ApiTags("Users API")
 @Controller("users")
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({ summary: "유저 생성 API", description: "유저를 생성합니다." })
+  @ApiCreatedResponse({ description: "유저를 생성합니다.", type: CreateUserResponseDto })
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  create(@Body() createUserDto: CreateUserDto): Promise<CreateUserResponseDto> {
     return this.usersService.create(createUserDto);
   }
 
+  @ApiOperation({ summary: "유저 목록 조회 API", description: "유저 목록을 조회합니다." })
+  @ApiOkResponse({ description: "유저 목록을 조회합니다.", type: Users, isArray: true })
   @Get()
-  findAll() {
+  findAll(): Promise<Users[]> {
     return this.usersService.findAll();
   }
 
+  @ApiOperation({ summary: "유저 조회 API", description: "유저 정보를 조회합니다." })
+  @ApiOkResponse({ description: "유저 정보를 조회합니다.", type: Users })
   @Get(":id")
-  findOne(@Param("id") id: string) {
-    return this.usersService.findOne(+id);
+  findOneById(@Param("id") id: string): Promise<Users> {
+    return this.usersService.findOneById(+id);
   }
 
+  @ApiOperation({ summary: "유저 삭제 API", description: "유저를 삭제합니다." })
+  @ApiOkResponse({ description: "유저를 삭제합니다.", type: DeleteUserResponseDto })
   @Delete(":id")
-  remove(@Param("id") id: string) {
+  remove(@Param("id") id: string): Promise<DeleteUserResponseDto> {
     return this.usersService.remove(+id);
   }
 }
