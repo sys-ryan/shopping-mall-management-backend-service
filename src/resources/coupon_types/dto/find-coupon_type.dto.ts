be851114a86310758a1ca24c07a160840b4c8eea
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-13 23:49:48 +0900

    feat: 쿠폰 유형별 쿠폰 사용 횟수, 총 할인액 정보 계산

--- src/resources/coupon_types/dto/find-coupon_type.dto.ts
+++ src/resources/coupon_types/dto/find-coupon_type.dto.ts
@@ -1,6 +1,10 @@
 import { IsEnum, IsNotEmpty, IsOptional, IsString } from "class-validator";
 import { COUPONE_TYPE_ENUM } from "src/common/enums";
+import { Coupons } from "src/resources/coupons/entities/coupons.entity";
 
+/**
+ * 쿠폰 유형 목록 조회시 검색 필터
+ */
 export class FindCouponTypeDto {
   @IsOptional()
   @IsEnum(COUPONE_TYPE_ENUM)
@@ -10,4 +14,20 @@ export class FindCouponTypeDto {
   @IsString()
   @IsNotEmpty()
   name: string;
+}
+
+export class FindCouponTypeResponseDto {
+  id: number;
+
+  name: string;
+
+  type: COUPONE_TYPE_ENUM;
+
+  discountValue: number;
+
+  coupons: Coupons[];
+
+  usedCount: number;
+
+  totalDiscountAmount: number;
 }
