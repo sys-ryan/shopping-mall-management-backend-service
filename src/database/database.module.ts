26fbd04423f7aa4e3cbb90885804b4d4942aa3fb
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-14 23:27:44 +0900

    test: e2e Test 수행

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,6 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
   imports: [
@@ -8,15 +8,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: ['dist/**/*.entity{.ts,.js}'],
+        type: "mysql",
+        host: configService.get("DB_HOST"),
+        port: configService.get("DB_PORT"),
+        username: configService.get("DB_USERNAME"),
+        password: configService.get("DB_PASSWORD"),
+        database: configService.get("DB_NAME"),
+        entities: ["dist/**/*.entity{.ts,.js}"],
         synchronize: true,
-        logging: true,
+        logging: process.env.NODE_ENV === "test" ? false : true,
+        autoLoadEntities: process.env.NODE_ENV === "test" ? true : false,
       }),
     }),
   ],
