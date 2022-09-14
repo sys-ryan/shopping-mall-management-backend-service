a3185dd3f7620f364da114b6fa0758dadd4b7236
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-14 23:27:44 +0900

    test: e2e Test 수행

--- test/coupons.e2e-spec.ts
+++ test/coupons.e2e-spec.ts
@@ -3,6 +3,12 @@ import { INestApplication } from "@nestjs/common";
 import * as request from "supertest";
 import { AppModule } from "./../src/app.module";
 import { setupApp } from "./setup-app";
+import { COUPONE_TYPE_ENUM } from "../src/common/enums";
+import { CouponTypes } from "src/resources/coupon_types/entities/coupon_types.entity";
+import { CreateCouponDto } from "src/resources/coupons/dto/create-coupon.dto";
+import { Coupons } from "src/resources/coupons/entities/coupons.entity";
+import { CreateOrderDto } from "src/resources/orders/dto/create-order.dto";
+import { Orders } from "src/resources/orders/entities/orders.entity";
 
 describe("Coupons (e2e)", () => {
   let app: INestApplication;
@@ -17,7 +23,142 @@ describe("Coupons (e2e)", () => {
     await app.init();
   });
 
-  it("/ (GET)", () => {
-    return request(app.getHttpServer()).get("/api/v1").expect(200).expect("Hello World!");
+  describe("쿠폰", () => {
+    it("쿠폰 타입 목록 조회 기능 검증", async () => {
+      let response = await request(app.getHttpServer())
+        .post(`/api/v1/coupon-types`)
+        .send({ name: "배송비 할인 쿠폰", type: COUPONE_TYPE_ENUM.SHIPPING_FEE, discountValue: 1 });
+      response = await request(app.getHttpServer())
+        .post(`/api/v1/coupon-types`)
+        .send({ name: "정액 할인 쿠폰", type: COUPONE_TYPE_ENUM.FIXED_VALUE, discountValue: 3000 });
+
+      response = await request(app.getHttpServer()).get(`/api/v1/coupon-types`);
+      const couponTypes: CouponTypes[] = response.body;
+
+      expect(couponTypes).toHaveLength(2);
+      expect(couponTypes[0].type).toEqual(COUPONE_TYPE_ENUM.SHIPPING_FEE);
+    });
+
+    it("쿠폰 사용에 따른 할인 적용 기능검증 (배송비 할인)", async () => {
+      // 배송비 할인 쿠폰 타입 생성
+      let response = await request(app.getHttpServer())
+        .post(`/api/v1/coupon-types`)
+        .send({ name: "배송비 할인 쿠폰", type: COUPONE_TYPE_ENUM.SHIPPING_FEE, discountValue: 1 });
+      response = await request(app.getHttpServer()).get(`/api/v1/coupon-types`);
+      const couponType: CouponTypes = response.body[0];
+
+      // 배송비 할인 쿠폰 발행 (1년 후 후 만료)
+      const expiresAt = new Date();
+      expiresAt.setMonth(expiresAt.getFullYear() + 3);
+
+      response = await request(app.getHttpServer())
+        .post(`/api/v1/coupons`)
+        .send({ couponTypeId: couponType.id, expiresAt } as CreateCouponDto);
+
+      const coupon: Coupons = response.body;
+
+      // 쿠폰 적용 주문 생성
+      const city = "Unique City";
+      response = await request(app.getHttpServer())
+        .post(`/api/v1/orders`)
+        .send({
+          userId: 1,
+          quantity: 5,
+          originalPrice: 100,
+          countryCode: "KR",
+          buyrCity: city,
+          buyrZipx: "05003",
+          vccode: 82,
+          couponCode: coupon.couponCode,
+        } as CreateOrderDto);
+      expect(response.statusCode).toBe(201);
+
+      response = await request(app.getHttpServer()).get(`/api/v1/orders?buyrCity=${city}`);
+
+      const order: Orders = response.body[0];
+      expect(order.originalDeliveryCost).not.toEqual(order.finalDeliveryCost);
+    });
+  });
+
+  it("쿠폰 사용에 따른 할인 적용 기능검증 (상품 가격 정액 할인)", async () => {
+    const discountValue = 10;
+    // 배송비 할인 쿠폰 타입 생성
+    let response = await request(app.getHttpServer())
+      .post(`/api/v1/coupon-types`)
+      .send({ name: "정액 할인 쿠폰", type: COUPONE_TYPE_ENUM.FIXED_VALUE, discountValue });
+    response = await request(app.getHttpServer()).get(`/api/v1/coupon-types`);
+    const couponType: CouponTypes = response.body[0];
+
+    // 배송비 할인 쿠폰 발행 (1년 후 후 만료)
+    const expiresAt = new Date();
+    expiresAt.setMonth(expiresAt.getFullYear() + 3);
+
+    response = await request(app.getHttpServer())
+      .post(`/api/v1/coupons`)
+      .send({ couponTypeId: couponType.id, expiresAt } as CreateCouponDto);
+
+    const coupon: Coupons = response.body;
+
+    // 쿠폰 적용 주문 생성
+    const city = "Unique City";
+    response = await request(app.getHttpServer())
+      .post(`/api/v1/orders`)
+      .send({
+        userId: 1,
+        quantity: 5,
+        originalPrice: 100,
+        countryCode: "KR",
+        buyrCity: city,
+        buyrZipx: "05003",
+        vccode: 82,
+        couponCode: coupon.couponCode,
+      } as CreateOrderDto);
+    expect(response.statusCode).toBe(201);
+
+    response = await request(app.getHttpServer()).get(`/api/v1/orders?buyrCity=${city}`);
+
+    const order: Orders = response.body[0];
+    expect(+order.originalPrice).toEqual(+order.finalPrice + discountValue);
+  });
+
+  it("쿠폰 사용에 따른 할인 적용 기능검증 (상품 가격 % 할인)", async () => {
+    const discountValue = 10;
+    // 배송비 할인 쿠폰 타입 생성
+    let response = await request(app.getHttpServer())
+      .post(`/api/v1/coupon-types`)
+      .send({ name: "정액 할인 쿠폰", type: COUPONE_TYPE_ENUM.PERCENTAGE, discountValue });
+    response = await request(app.getHttpServer()).get(`/api/v1/coupon-types`);
+    const couponType: CouponTypes = response.body[0];
+
+    // 배송비 할인 쿠폰 발행 (1년 후 후 만료)
+    const expiresAt = new Date();
+    expiresAt.setMonth(expiresAt.getFullYear() + 3);
+
+    response = await request(app.getHttpServer())
+      .post(`/api/v1/coupons`)
+      .send({ couponTypeId: couponType.id, expiresAt } as CreateCouponDto);
+
+    const coupon: Coupons = response.body;
+
+    // 쿠폰 적용 주문 생성
+    const city = "Unique City";
+    response = await request(app.getHttpServer())
+      .post(`/api/v1/orders`)
+      .send({
+        userId: 1,
+        quantity: 5,
+        originalPrice: 100,
+        countryCode: "KR",
+        buyrCity: city,
+        buyrZipx: "05003",
+        vccode: 82,
+        couponCode: coupon.couponCode,
+      } as CreateOrderDto);
+    expect(response.statusCode).toBe(201);
+
+    response = await request(app.getHttpServer()).get(`/api/v1/orders?buyrCity=${city}`);
+
+    const order: Orders = response.body[0];
+    expect(+order.finalPrice).toEqual(+order.originalPrice * (1 - discountValue / 100));
   });
 });
--- test/orders.e2e-spec.ts
+++ test/orders.e2e-spec.ts
@@ -3,6 +3,17 @@ import { INestApplication } from "@nestjs/common";
 import * as request from "supertest";
 import { AppModule } from "./../src/app.module";
 import { setupApp } from "./setup-app";
+import { Orders } from "../src/resources/orders/entities/orders.entity";
+import { PAY_STATE_ENUM } from "../src/common/enums";
+import { Users } from "src/resources/users/entities/users.entity";
+
+const stringToDate = (date: string) => {
+  const year = +date.slice(0, 4);
+  const month = +date.slice(4, 6);
+  const day = +date.slice(6, 8);
+
+  return new Date(year, month - 1, day);
+};
 
 describe("Orders (e2e)", () => {
   let app: INestApplication;
@@ -16,8 +27,80 @@ describe("Orders (e2e)", () => {
     setupApp(app);
     await app.init();
   });
+  describe("주문 내역)", () => {
+    it("주문 내역 열람 기능 검증", async () => {
+      const response = await request(app.getHttpServer()).get("/api/v1/orders");
 
-  it("/ (GET)", () => {
-    return request(app.getHttpServer()).get("/api/v1").expect(200).expect("Hello World!");
+      expect(response.statusCode).toEqual(200);
+
+      const orders: Orders[] = response.body;
+      expect(orders[0].id).toBeDefined();
+    });
+
+    it("주문 내역 검색 기능 검증 (by 주문 상태)", async () => {
+      const response = await request(app.getHttpServer()).get("/api/v1/orders?payState=canceled");
+      expect(response.statusCode).toEqual(200);
+
+      let isOk = true;
+      const orders: Orders[] = response.body;
+      for (var i = 0; i < orders.length; i++) {
+        if (orders[i].payState !== PAY_STATE_ENUM.CANCELED) {
+          isOk = false;
+        }
+      }
+      expect(isOk).toBe(true);
+    });
+
+    it("주문 내역 검색 기능 검증 (by 시작일자, 종료일자)", async () => {
+      const start = "20220101";
+      const end = "20220201";
+      const response = await request(app.getHttpServer()).get(
+        `/api/v1/orders?startDate=${start}&endDate=${end}`
+      );
+
+      const startDate = stringToDate(start);
+      const endDate = stringToDate(end);
+
+      let isOk = true;
+      const orders: Orders[] = response.body;
+
+      for (var i = 0; i < orders.length; i++) {
+        const createdAt = new Date(orders[i].createdAt);
+
+        if (createdAt < startDate || createdAt > endDate) {
+          isOk = false;
+        }
+      }
+
+      expect(isOk).toBe(true);
+    });
+
+    it("주문 내역 검색 기능 (by 주문자 명)", async () => {
+      let response = await request(app.getHttpServer()).get(`/api/v1/users`);
+      const user: Users = response.body[0];
+
+      response = await request(app.getHttpServer()).get(`/api/v1/orders?buyrName=${user.name}`);
+      const order: Orders = response.body[0];
+
+      expect(order.user.name).toEqual(user.name);
+    });
+
+    it("주문 내역 검색 기능 (by 국가 코드)", async () => {
+      const countryCode = "BR"; // Brazil
+
+      const response = await request(app.getHttpServer()).get(
+        `/api/v1/orders?buyrCountry=${countryCode}`
+      );
+      const orders: Orders[] = response.body;
+
+      let isOk = true;
+      for (var i = 0; i < orders.length; i++) {
+        if (orders[i].country.countryCode !== countryCode) {
+          isOk = false;
+        }
+      }
+
+      expect(isOk).toBe(true);
+    });
   });
 });
