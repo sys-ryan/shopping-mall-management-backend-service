10e5c6fd24df3bb30c325c5ae15f21cf9ff0e388
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-14 19:48:08 +0900

    test: deliveries unit test 환경 세팅

--- src/resources/deliveries/deliveries.service.spec.ts
+++ src/resources/deliveries/deliveries.service.spec.ts
@@ -1,18 +1,44 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { DeliveriesService } from './deliveries.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { OrdersService } from "../orders/orders.service";
+import { DeliveriesService } from "./deliveries.service";
+import { Deliveries } from "./entities/deliveries.entity";
 
-describe('DeliveriesService', () => {
+const deliveries: Deliveries[] = [];
+
+const mockDeliveriesRepository = () => {
+  return { create: jest.fn(), save: jest.fn(), find: jest.fn(), findOne: jest.fn() };
+};
+
+const mockOrdersService = () => ({
+  findOnebyId: jest.fn(),
+});
+
+describe("DeliveriesService", () => {
   let service: DeliveriesService;
+  let deliveriesRepository: Repository<Deliveries>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [DeliveriesService],
+      providers: [
+        DeliveriesService,
+        {
+          provide: getRepositoryToken(Deliveries),
+          useValue: mockDeliveriesRepository(),
+        },
+        {
+          provide: OrdersService,
+          useValue: mockOrdersService(),
+        },
+      ],
     }).compile();
 
     service = module.get<DeliveriesService>(DeliveriesService);
+    deliveriesRepository = module.get(getRepositoryToken(Deliveries));
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
   });
 });
