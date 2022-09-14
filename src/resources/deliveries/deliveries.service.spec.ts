64a67f6bc3d26c3a10c928a454fd3ec66b8bcd19
Seong-Jin Kim <sys.ryan0902@gmail.com>
2022-09-14 20:03:22 +0900

    test: Delivery Unit Test
    - 주문 발송 처리(배송 정보 생성) 기능 검증
    - 배송 상태 업데이트 (배송 중, 배송 완료) 기능 검증
    - 배송 목록 조회 기능 검증

--- src/resources/deliveries/deliveries.service.spec.ts
+++ src/resources/deliveries/deliveries.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { getRepositoryToken } from "@nestjs/typeorm";
+import { DELIVERY_STATUS_ENUM, PAY_STATE_ENUM } from "../../common/enums";
 import { Repository } from "typeorm";
 import { OrdersService } from "../orders/orders.service";
 import { DeliveriesService } from "./deliveries.service";
@@ -8,12 +9,63 @@ import { Deliveries } from "./entities/deliveries.entity";
 const deliveries: Deliveries[] = [];
 
 const mockDeliveriesRepository = () => {
-  return { create: jest.fn(), save: jest.fn(), find: jest.fn(), findOne: jest.fn() };
+  return {
+    create: jest.fn().mockImplementation((order) => ({
+      id: 1,
+      status: DELIVERY_STATUS_ENUM.SHIPPING,
+      order,
+    })),
+    save: jest.fn((delivery) => {
+      let index: number;
+      for (var i = 0; i < deliveries.length; i++) {
+        if (deliveries[i].id === delivery.id) {
+          index = i;
+          break;
+        }
+      }
+
+      if (index) {
+        deliveries.splice(index, 1);
+      }
+      deliveries.push({
+        id: 1,
+        ...delivery,
+      });
+    }),
+    find: jest.fn().mockImplementation(() => deliveries),
+    findOne: jest.fn().mockImplementation((query) => {
+      const where = query.where;
+
+      let existingDelivery: Deliveries;
+      if (where.id) {
+        deliveries.forEach((delivery) => {
+          if (delivery.id === where.id) {
+            existingDelivery = delivery;
+          }
+        });
+      }
+
+      return existingDelivery;
+    }),
+  };
 };
 
 const mockOrdersService = () => ({
-  findOnebyId: jest.fn(),
+  findOneById: jest.fn().mockImplementation((id) => defaultOrderObject),
 });
+
+const defaultOrderObject = {
+  id: 1,
+  userId: 1,
+  quantity: 3,
+  countryCode: "US",
+  originalPrice: 100,
+  buyrCity: "New York",
+  buyrZipx: "12345",
+  couponCode: null,
+  vccode: 1,
+  payState: PAY_STATE_ENUM.COMPLETED,
+};
 
 describe("DeliveriesService", () => {
   let service: DeliveriesService;
@@ -38,7 +90,35 @@ describe("DeliveriesService", () => {
     deliveriesRepository = module.get(getRepositoryToken(Deliveries));
   });
 
+  afterEach(async () => {
+    deliveries.splice(0, deliveries.length);
+  });
+
   it("should be defined", () => {
     expect(service).toBeDefined();
+  });
+
+  it("주문 발송 처리(배송 정보 생성) 기능 검증", async () => {
+    await service.create({ orderId: 1 });
+
+    expect(deliveries).toHaveLength(1);
+    expect(deliveries[0].status).toEqual(DELIVERY_STATUS_ENUM.SHIPPING);
+  });
+
+  it("배송 상태 업데이트(배송중, 배송 완료) 기능 검증", async () => {
+    await service.create({ orderId: 1 });
+    expect(deliveries[0].status).toEqual(DELIVERY_STATUS_ENUM.SHIPPING);
+
+    await service.update(1, { status: DELIVERY_STATUS_ENUM.COMPLETED });
+    expect(deliveries[0].status).toEqual(DELIVERY_STATUS_ENUM.COMPLETED);
+  });
+
+  it("배송 목록 조회 기능 검증", async () => {
+    await service.create({ orderId: 1 });
+    await service.create({ orderId: 2 });
+    await service.create({ orderId: 3 });
+
+    const fetchedDeliveries = await service.findAll({ status: null });
+    expect(fetchedDeliveries).toHaveLength(3);
   });
 });
